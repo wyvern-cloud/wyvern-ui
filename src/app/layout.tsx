0ab50269940dea75bf75d41d21af234dc3ba9ec5
Colton Wolkins (Laptop) <colton@indicio.tech>
2024-03-17 06:50:32 -0600

    feat: PWA test
    Signed-off-by: Colton Wolkins (Laptop) <colton@indicio.tech>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <script src="/pwa-script.js" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
