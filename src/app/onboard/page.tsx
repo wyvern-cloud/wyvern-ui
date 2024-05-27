d23e7d8b39caeb32724e3e458f845a68dcf163b7
Colton Wolkins (Laptop) <colton@indicio.tech>
2024-05-27 11:51:41 -0600

    feat: move onboarding to /onboard
    Signed-off-by: Colton Wolkins (Laptop) <colton@indicio.tech>

--- src/app/onboard/page.tsx
+++ src/app/onboard/page.tsx
@@ -23,9 +23,9 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<form onSubmit={onSubmit}>
-				<input type="text" name="name" placeholder="Your Name" onChange={(e) => setName(e.target.value)} />
-				<button type="submit" disabled={isLoading || !name}>
+			<form onSubmit={onSubmit} className="text-center flex gap-1 flex-col">
+				<input type="text" name="name" className="rounded-md" placeholder="Your Name" onChange={(e) => setName(e.target.value)} />
+				<button type="submit" className="rounded-md bg-indigo-600 px-3 py-2 text-md font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" disabled={isLoading || !name}>
 					{isLoading ? 'Loading...' : 'Get Started'}
 				</button>
 			</form>
