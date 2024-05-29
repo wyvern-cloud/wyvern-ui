ba748e6e7f230e248a40f6dce02f05f709843d32
Colton Wolkins (Laptop) <colton@indicio.tech>
2024-05-28 23:56:09 -0600

    feat: Add nav bar header to home/landing page
    Signed-off-by: Colton Wolkins (Laptop) <colton@indicio.tech>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,16 +11,47 @@ export default function Home() {
   return (
 		<div className="flex flex-col">
 			<header>
-				<nav>
-					<a href="https://github.com/wyvern-cloud/wyvern-ui" className="github-corner" aria-label="View source on GitHub">
+				
+
+				<nav className="bg-white border-gray-200 dark:bg-gray-900 z-40 relative">
+					<div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 relative">
+						<a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+								<img src="/pwa/icons-vector.svg" className="h-8" style={{filter: "invert(56%) sepia(44%) saturate(4724%) hue-rotate(160deg) brightness(92%) contrast(101%)"}} alt="Wyvrn Logo" />
+								<span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Wyvrn</span>
+						</a>
+						<div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+								<button type="button" onClick={() => router.push("/onboard")} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Open Wyvrn</button>
+								<button data-collapse-toggle="navbar-cta" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
+									<span className="sr-only">Open main menu</span>
+									<svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+											<path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
+									</svg>
+							</button>
+						</div>
+						<div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
+							<ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+								<li>
+									<a href="#top" className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500" aria-current="page">Home</a>
+								</li>
+								<li>
+									<a href="#content" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+								</li>
+								<li>
+									<a href="https://github.com/wyvern-cloud/wyvern-ui" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Github</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<a href="https://github.com/wyvern-cloud/wyvern-ui" className="github-corner relative min-w-full block" aria-label="View source on GitHub">
 						<svg width="80" height="80" viewBox="0 0 250 250" style={{fill: "#151513", color: "#fff", position: "absolute", top: 0, border: 0, right: 0 }} aria-hidden="true">
 							<path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path>
 							<path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style={{transformOrigin: "130px 106px"}} className="octo-arm"></path>
-							<path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path>
+							<path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" className="octo-body"></path>
 						</svg>
 					</a>
 					<style dangerouslySetInnerHTML={{__html: `.github-corner:hover .octo-arm{animation:octocat-wave 560ms ease-in-out}@keyframes octocat-wave{0%,100%{transform:rotate(0)}20%,60%{transform:rotate(-25deg)}40%,80%{transform:rotate(10deg)}}@media (max-width:500px){.github-corner:hover .octo-arm{animation:none}.github-corner .octo-arm{animation:octocat-wave 560ms ease-in-out}}`}} />
 				</nav>
+
 				<div className={`min-h-[50dvh] ${styles["hero-image"]}`}>
 					<div className={styles["hero-text"]}>
 						<h1 className="text-8xl font-extrabold pb-8">Wyvrn</h1>
@@ -28,7 +59,8 @@ export default function Home() {
 					</div>
 				</div>
 			</header>
-			<main className="flex flex-col items-center justify-between p-8">
+
+			<main className="flex flex-col items-center justify-between p-8" id="content">
 				<div className="flex min-w-max gap-16 justify-evenly border-4 border-slate-500 rounded-xl p-16">
 					<div>
 						<h2 className="text-2xl">A new Era of Messaging</h2>
