d0b3ab537d1a5d319955e28f12c7a346b248a55b
Colton Wolkins (Laptop) <colton@indicio.tech>
2024-06-03 22:02:33 -0600

    fix: remove useless line?
    Signed-off-by: Colton Wolkins (Laptop) <colton@indicio.tech>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,6 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 		"./node_modules/flowbite-react/lib/**/*.js",
-    "./out/**/*.{js,ts,jsx,tsx,mdx}",
   ],
 	darkMode: 'media',
   theme: {
