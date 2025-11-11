381f4b1cb2b5823b1b1e017ae7ff73b4e30d13bb
Colton Wolkins (Laptop) <colton@indicio.tech>
2025-11-10 23:29:04 -0700

    feat: Allow users to submit by pressing enter
    Signed-off-by: Colton Wolkins (Laptop) <colton@indicio.tech>

--- src/wyvrn/components/chat/MessageInput.jsx
+++ src/wyvrn/components/chat/MessageInput.jsx
@@ -2,27 +2,40 @@ import m from "mithril";
 import styles from "../../server-list.module.css";
 import w from "../../agent";
 
+const submitOnEnter = async (e, sendMessage) => {
+  if (e.key === "Enter" && !e.shiftKey) {
+    e.preventDefault();
+    await submitMessage(e, sendMessage);
+  }
+}
+
+const submitMessage = async (e, sendMessage) => {
+  const form = e.target.form;
+  const textbox = form.getElementsByTagName("textarea")[0];
+  const message = textbox.value;
+  textbox.disabled = true;
+  if (sendMessage && !(await sendMessage(message))) {
+    textbox.disabled = false;
+    textbox.focus();
+    return;
+  }
+  textbox.value = '';
+  textbox.disabled = false;
+  textbox.focus();
+  return;
+  worker.postMessage({
+    action: 'demo',
+  });
+}
+
 const MessageInput = {
   view: ({ attrs }) => (
     <div class={styles.red}>
       <form class={styles.textbox}>
-        <textarea class={styles.red}></textarea>
+        <textarea class={styles.userinput} placeholder="Message Goes Here" onkeydown={async (e) => submitOnEnter(e, attrs.sendMessage)}></textarea>
         <button type="submit" value="Submit" onclick={async (e) => {
           e.preventDefault();
-          const form = e.target.form;
-          const textbox = form.getElementsByTagName("textarea")[0];
-          const message = textbox.value;
-          textbox.disabled = true;
-          if (attrs.sendMessage && !(await attrs.sendMessage(message))) {
-            textbox.disabled = false;
-            return;
-          }
-          textbox.value = '';
-          textbox.disabled = false;
-          return;
-          worker.postMessage({
-            action: 'demo',
-          });
+          await submitMessage(e, attrs.sendMessage);
         }}>Send</button>
       </form>
     </div>
